073d465e7966644ea752c54068a1d1747351aa59
ramyasri <ramyasri@edtex.in>
2025-02-12 21:05:41 +0530

    initial files

---
+++ src/main/java/com/example/LeaveTracker/LeaveTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.example.LeaveTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveTrackerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LeaveTrackerApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/example/LeaveTracker/LeaveTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeaveTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
